2e54b8db5133bb311e0b96fbb4fa41c06c2ab88d
yechao <best_yc@163.com>
2021-02-09 19:43:14 +0800

    toolbar

--- app/src/main/java/com/yechaoa/materialdesign/activity/ToolbarActivity.kt
+++ app/src/main/java/com/yechaoa/materialdesign/activity/ToolbarActivity.kt
@@ -1,8 +1,6 @@
 package com.yechaoa.materialdesign.activity
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -17,48 +15,38 @@ abstract class ToolbarActivity : AppCompatActivity() {
         setContentView(getLayoutId())
         mToolbar = findViewById(R.id.toolbar)
 
+        setListener()
+        setToolbar()
+        initView()
+    }
+
+    private fun setListener() {
+
+        /**
+         * toolbar上back的事件处理
+         */
         mToolbar.setNavigationOnClickListener {
             finish()
         }
 
-        //mToolbar.inflateMenu(R.menu.menu_toolbar);
-
-        setToolbar()
-        initView()
+        /**
+         * toolbar上menu的事件处理
+         */
+        mToolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.menu_share -> {
+                    Toast.makeText(this, "分享", Toast.LENGTH_SHORT).show()
+                }
+                R.id.menu_settings -> {
+                    Toast.makeText(this, "设置", Toast.LENGTH_SHORT).show()
+                }
+            }
+            return@setOnMenuItemClickListener true
+        }
     }
 
     protected abstract fun getLayoutId(): Int
     protected abstract fun setToolbar()
     protected abstract fun initView()
 
-    /**
-     * 引入toolbar上的menu
-     */
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_toolbar, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    /**
-     * toolbar上menu的事件处理
-     */
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            android.R.id.home -> {
-                //Toast.makeText(this, "back", Toast.LENGTH_SHORT).show();
-                finish()
-                true
-            }
-            R.id.menu_share -> {
-                Toast.makeText(this, "分享", Toast.LENGTH_SHORT).show()
-                true
-            }
-            else -> {
-                // If we got here, the user's action was not recognized.
-                // Invoke the superclass to handle it.
-                Toast.makeText(this, "设置", Toast.LENGTH_SHORT).show()
-                super.onOptionsItemSelected(item)
-            }
-        }
-    }
 }
